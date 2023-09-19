875543cdf0c27313b9081aa0aacf0a2175ccc55c
Newjackswing <youneverforgetmyemailaddress@gmail.com>
2023-09-19 13:17:59 +0900

    first commit

---
+++ src/main/java/com/example/test/geoserver/GeoServerTestController.java
@@ -0,0 +1,23 @@
+package com.example.test.geoserver;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class GeoServerTestController {
+
+    @RequestMapping("/test")
+    private String geoserverTest(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Model model
+    ){
+        model.addAttribute("paramGeo", "HELLO GEOSERVER");
+        return "/geoserver";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/geoserver.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yyugh
+  Date: 2023-09-19
+  Time: 오전 11:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>GEOSERVER TEST</title>
+</head>
+<body>
+    <h2>${paramGeo}</h2>
+</body>
+</html>
